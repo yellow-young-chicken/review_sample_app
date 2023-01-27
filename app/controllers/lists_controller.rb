26060fa5551a866a059b0fe621adf8e59fa808b1
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-27 13:04:50 +0000

    done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿が成功しました。"
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -27,10 +31,16 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to "/lists"
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
